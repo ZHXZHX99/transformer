d480857ac69600798a996e10cc92b28c7ac082b9
ZHXZHX99 <1394477532@qq.com>
2021-09-15 14:42:46 +0800

    about the theory

---
+++ Positional_Encoding.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def get_positional_encoding(max_seq_len, embed_dim):
+    # 初始化一个positional encoding
+    # embed_dim: 字嵌入的维度
+    # max_seq_len: 最大的序列长度
+    positional_encoding = np.array([
+        [pos / np.power(10000, 2 * i / embed_dim) for i in range(embed_dim)]
+        if pos != 0 else np.zeros(embed_dim) for pos in range(max_seq_len)])
+    
+    positional_encoding[1:, 0::2] = np.sin(positional_encoding[1:, 0::2])  # dim 2i 偶数
+    positional_encoding[1:, 1::2] = np.cos(positional_encoding[1:, 1::2])  # dim 2i+1 奇数
+    return positional_encoding
+
+
+positional_encoding = get_positional_encoding(max_seq_len=100, embed_dim=16)
+plt.figure(figsize=(10, 10))
+sns.heatmap(positional_encoding)
+plt.title("Sinusoidal Function")
+plt.xlabel("hidden dimension")
+plt.ylabel("sequence length")
+plt.figure(figsize=(8, 5))
+plt.plot(positional_encoding[1:, 1], label="dimension 1")
+plt.plot(positional_encoding[1:, 2], label="dimension 2")
+plt.plot(positional_encoding[1:, 3], label="dimension 3")
+plt.legend()
+plt.xlabel("Sequence length")
+plt.ylabel("Period of Positional Encoding")
+plt.show()
+
